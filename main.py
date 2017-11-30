d859e7d6cdf0a76da7ed8dba709a93c53cfa77f3
Lancat <lancat1499@gmail.com>
2017-11-30 18:53:09 +0800

    加入座位列表

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ from data import persent
 class launcher:
     def __init__(self):
         with open('test_data','r') as file:
-            self.w_h = (file.readline().split('=')[1].split('*'))
+            w,h = (file.readline().split('=')[1].split('*'))
             print(self.w_h)
             self.people = []
             for i in file.readlines():
                 n,p = i.split(':')[0],i.split(':')[1].split(',')
                 self.people.append(persent(n,p))
         print(self.people)
+        self.seat = []
+        for i in range(int(w)):
+            for j in range(int(h)):
+                self.seat.append({'w':i,'h':j,'free':False})
+        print(self.seat)
     def start(self):
         pass
